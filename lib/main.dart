90e07b7c703c445870ffdad9a190ecab58c71898
Shaurya0087 <shaurya0087@gmail.com>
2023-04-26 16:56:22 +0530

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/pages/login_page.dart';
-
-import 'pages/home_page.dart';
-
-void main() {
+import 'package:yt/pages/home_page.dart';
+import 'package:yt/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+void main()
+{
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget
+{
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
+
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark),
-      initialRoute: "/home",
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/",
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => HomePage(),
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatelessWidget
+{
+
   final int days = 30;
   final String name = "Codeui";
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: Text("CatalogApp"),
       ),
       body: Center(
         child: Container(
-          child: Text("welcome $days into $name "),
+          child: Text("welcome to $days of flutter $name"),
         ),
       ),
       drawer: Drawer(),
